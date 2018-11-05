8a99386f5f710695dc0387fa5e299e790a22e6aa
sg3957 <sg3957@nyu.edu>
2018-11-05 17:07:52 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  # get 'songs/show'
+  #
+  # get 'songs/edit'
+  #
+  # get 'songs/update'
+  #
+  # get 'songs/new'
+  #
+  # get 'songs/create'
+
+  resources :songs
 end
